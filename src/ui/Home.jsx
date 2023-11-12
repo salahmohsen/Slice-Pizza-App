865178be1e197f7e75def9454d5b8f14fae0e972
salahmohsen <salahmmohsen@gmail.com>
2023-11-12 17:23:44 +0200

    Home - 1

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,21 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <div className="flex items-center justify-center">
+        <p>The Best Pizza For </p>
+        <form>
+          <input
+            type="text"
+            placeholder={"Your Name?"}
+            id="search"
+            className="bg-red hover:bg-bg-input text-white rounded-full hover:pr-5  py-2 text-sm uppercase placeholder:italic pl-12  outline-0 placeholder:capitalize font-logo w-20 hover:w-72 ease-in-out duration-500 "
+          />
+          <button className="bg-red ">Start Ordering</button>
+        </form>
+      </div>
+      <p>Straight out of the oven, straight to you.</p>
+    </div>
+  );
 }
 
 export default Home;
