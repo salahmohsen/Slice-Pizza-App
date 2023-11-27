ccb236ed4174960bad5dad5520a012fb1076a45d
salahmohsen <salahmmohsen@gmail.com>
2023-11-27 17:43:31 +0200

    loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,26 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigation } from "react-router-dom";
 import Logo from "./Logo";
 import Search from "./Search";
-import User from "./User";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div className="m-auto w-5/6">
-      <div className=" flex flex-col items-center justify-between sm:flex-row">
-        <Link to={"/"}>
-          <Logo />
-        </Link>
-        <Search />
+    <>
+      {isLoading && <Loader />}
+      <div className="m-auto w-5/6">
+        <div className=" flex flex-col items-center justify-between sm:flex-row">
+          <Link to={"/"}>
+            <Logo />
+          </Link>
+          <Search />
+        </div>
+        <div>
+          <Outlet />
+        </div>
       </div>
-      <div>
-        <Outlet />
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,13 @@
+import { GiFullPizza } from "react-icons/gi";
+
 function Loader() {
-  return <div></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center text-9xl text-white backdrop-blur-lg">
+      <span className=" animate-spin-slow">
+        <GiFullPizza />
+      </span>
+    </div>
+  );
 }
 
 export default Loader;
